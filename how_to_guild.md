9ec5982561cba8b0ff43f0f11075faa587d29ccd
Olabarakat45 <barakatadisa45@gmail.com>
2023-03-06 15:14:46 +0100

    new updates

---
+++ how_to_guild.md
@@ -0,0 +1,9 @@
+# How to use this app to perform kruskal's algorithm
+I will be showing you step by step guild on how to use our app to perform kruskal's algorithm. So lets dive into it.
+
+
+**Step 1**
+- Input all nodes seperated with space in the required input field
+![k1.pmg](./img/k1.PNG)
+
+
--- kruskals.js
+++ kruskals.js
@@ -1,21 +1,21 @@
 let numEdges = 0;
 
 function addEdge() {
-   numEdges++
+  numEdges++
 
-   let edgeDiv = document.createElement("div");
-   edgeDiv.classList.add("edge-input");
+  let edgeDiv = document.createElement("div");
+  edgeDiv.classList.add("edge-input");
 
-   edgeDiv.innerHTML =
-   '<label  for="source-' + numEdges + '">Enter the source node of edge ' + numEdges +':</label>' +
-   '<input class="source-" type="text" id="source-' + numEdges + '" name="source-' + numEdges + '" oninput="this.value = this.value.toUpperCase()" required />' +
+  edgeDiv.innerHTML =
+  '<label  for="source-' + numEdges + '">Enter the source node of edge ' + numEdges +':</label>' +
+  '<input class="source-" type="text" id="source-' + numEdges + '" name="source-' + numEdges + '" oninput="this.value = this.value.toUpperCase()" required />' +
 
-   '<label class="target-" for="target-' + numEdges + '">Enter the target node of edge ' + numEdges + ':</label>' +
-   '<input type="text" id="target-' + numEdges + '" name="target-' + numEdges +'" oninput="this.value = this.value.toUpperCase()" required />' +
+  '<label class="target-" for="target-' + numEdges + '">Enter the target node of edge ' + numEdges + ':</label>' +
+  '<input type="text" id="target-' + numEdges + '" name="target-' + numEdges +'" oninput="this.value = this.value.toUpperCase()" required />' +
 
-   '<label class="weight-" for="weight-' + numEdges + '">Enter the weight of edge ' + numEdges + ':</label>' +
-   '<input type="number" id="weight-' + numEdges + '" name="weight-' + numEdges + '" oninput="this.value = this.value.toUpperCase()" required />' + '<br />';
-   document.getElementById("edges").appendChild(edgeDiv);
+  '<label class="weight-" for="weight-' + numEdges + '">Enter the weight of edge ' + numEdges + ':</label>' +
+  '<input type="number" id="weight-' + numEdges + '" name="weight-' + numEdges + '" oninput="this.value = this.value.toUpperCase()" required />' + '<br />';
+  document.getElementById("edges").appendChild(edgeDiv);
 }
 
 
@@ -57,11 +57,14 @@ function kruskal(edges, nodes) {
     }
   }
 
-  console.log(minimumSpanningTree);
+  //calling function to show result
+  showResult(minimumSpanningTree);
 }
 
 
 function runKruskal() {
+
+
 
   // getting nodes
   let numNodes = document.getElementById("nodes").value;
@@ -82,4 +85,36 @@ function runKruskal() {
   //calling krukals Algorithm fouction
   kruskal(edges, nodes);
     
+}
+
+
+
+function showResult(minimumSpanningTree){
+
+  let minimalLength = 0;
+  document.getElementById("close").style.display = "block";
+
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("resultDiv");
+
+  for (let i = 0; i < minimumSpanningTree.length; i++) {
+    minimalLength += parseInt(minimumSpanningTree[i].weight);
+    
+  }
+
+  //check if result div is display none
+  if (document.getElementById("result").style.display == "none") {
+    document.getElementById("result").style.display = "block";
+  }
+
+  //output minimal spanning tree length
+  newDiv.innerHTML = "<p> The Minimum Spanning Tree Lenght: </br> <h1>" + minimalLength + "</h1></p>";
+  document.getElementById("result").appendChild(newDiv);
+}
+
+
+// close result block
+function closeResult(){
+  document.getElementById("result").style.display = "none";
+  window.location.href = "/";
 }
